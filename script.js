7d1ddab4144748ddb506f3f626f716392878aa41
Roman <bolist@mail.ru>
2018-08-07 15:59:37 +0300

    all in different files

---
+++ script.js
@@ -0,0 +1,15 @@
+function perimetr() {
+    var a = parseInt(document.f1.a.value);
+    var b = parseInt(document.f1.b.value);
+    var c = parseInt(document.f1.c.value);
+    var per = a + b + c;
+    alert(per);
+}
+function ploshcha() {
+        var a = parseInt(document.f1.a.value);
+        var b = parseInt(document.f1.b.value);
+        var c = parseInt(document.f1.c.value);
+        var d = a + b + c;
+        var per = d/2;
+        alert(per);
+}
