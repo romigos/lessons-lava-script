a9c49e3384f2137979b77faab0aef53e1a46e559
Roman <bolist@mail.ru>
2018-08-15 12:53:21 +0300

    new sum

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 //js 12 площа і периметр трикутника
 function perimetr() {
-    var a = parseInt(document.f1.a.value);
-    var b = parseInt(document.f1.b.value);
-    var c = parseInt(document.f1.c.value);
-    var per = a + b + c;
-    alert(per);
+    var a=document.forma1.a.value;
+    var b=document.forma1.b.value;
+    var c=document.forma1.c.value;
+    var p=a*b*c;
+   document.forma1.res.value=p;
 }
 function ploshcha() {
-        var a = parseInt(document.f1.a.value);
-        var b = parseInt(document.f1.b.value);
-        var c = parseInt(document.f1.c.value);
-        var d = a + b + c;
-        var per = d/2;
-        alert(per);
+    var a=document.forma1.a.value;
+    var b=document.forma1.b.value;
+    var c=document.forma1.c.value;
+    var d=a+b+c;
+    var s=d/2;
+    document.forma1.res.value=s;
 }
