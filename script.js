f9d205913868a2984f0b73f2dfdbe00ffdc089e0
Roman <bolist@mail.ru>
2018-09-06 17:49:00 +0300

    task2

--- script.js
+++ script.js
@@ -1,19 +1,34 @@
 //js 12 площа і периметр трикутника
 function perimeterTriangle() {
-    var st1 = parseInt(document.forma1.t1.value);
-    var st2 = parseInt(document.forma1.t2.value);
-    var st3 = parseInt(document.forma1.t3.value);
-    var perimetr = st1 + st2 + st3;
-    var res = perimetr;
-        //alert(perimetr);//
-    document.forma1.res.value;
+    var st1,st2,st3,perimetr;
+    st1 = parseInt(document.forma1.t1.value);
+    st2 = parseInt(document.forma1.t2.value);
+    st3 = parseInt(document.forma1.t3.value);
+    perimetr = st1 + st2 + st3;
+    document.getElementById('res1').innerHTML = perimetr;
 }
 function areaTriangle() {
-    var st1 = parseInt(document.forma1.t1.value);
-    var st2 = parseInt(document.forma1.t2.value);
-    var st3 = parseInt(document.forma1.t3.value);
-    var d = a + b + c;
-    var s = d / 2;
-    alert(s);
-    document.forma1.res.value = s;
+    var st1,st2,st3,sguare;
+    st1 = parseInt(document.forma1.t1.value);
+    st2 = parseInt(document.forma1.t2.value);
+    st3 = parseInt(document.forma1.t3.value);
+    sguare = (st1 + st2 + st3)/2;
+    document.getElementById('res2').innerHTML = sguare;
+}
+function terra() {
+    alert('РОМА УЧИ JAVASCRIPT')
+}
+function plus() {
+    var num1,num2,result;
+    num1 = parseInt(document.getElementById('n1').value);
+    num2 = parseInt(document.getElementById('n2').value);
+    result = num1 + num2;
+    document.getElementById('out').innerHTML = result;
+}
+function minus() {
+    var num1,num2,result;
+    num1 = parseInt(document.getElementById('n1').value);
+    num2 = parseInt(document.getElementById('n2').value);
+    result = num1 — num2;
+    document.getElementById('out').innerHTML = result;
 }
